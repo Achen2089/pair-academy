7c8714f186d121882f6c31af0f84413f102c2c6a
Achen2089 <anthonychen2089@gmail.com>
2023-11-30 12:15:35 -0800

    integrate api to chat

--- src/components/ChatWindow.tsx
+++ src/components/ChatWindow.tsx
@@ -3,18 +3,49 @@ import axios from 'axios';
 
 
 export default function ChatWindow() {
+  const apiBaseUrl = import.meta.env.VITE_API_BASE_URL;
   const [messages, setMessages] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState('');
+  const [isSending, setIsSending] = useState(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
 
+  const displayResponseWordByWord = (responseMessage: string, index = 0, allWords : string[]) => {
+    if (index < allWords.length) {
+      setMessages(messages => {
+        const newMessages = [...messages];
+        newMessages[newMessages.length - 1] += (index === 0 ? "" : " ") + allWords[index];
+        return newMessages;
+    });
+      setTimeout(() => displayResponseWordByWord(responseMessage, index + 1, allWords), 50);
+    } 
+    else {
+      setIsSending(false);
+    }
+  };
+
+  const sendMessageToServer = async (userInput: string) => {
+    setIsSending(true);
+    try {
+      const response = await axios.post(`${apiBaseUrl}/chat`, {message : userInput});
+      if (response.data && response.data.message) {
+        setMessages(messages => [...messages, ""]);
+        displayResponseWordByWord(response.data.message, 0, response.data.message.split(' '));
+      }
+    } catch (error) {
+      console.error("Error sending message to server", error)
+      setIsSending(false);
+    }
+  };
+
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     
     event.preventDefault();
-    if (inputValue.trim() !== '') {
-      setMessages([...messages, inputValue]);
+    if (inputValue.trim() !== '' && !isSending) {
+      setMessages(messages => [...messages, inputValue]);
+      sendMessageToServer(inputValue.trim());
       setInputValue('');
     }
     
@@ -29,7 +60,7 @@ export default function ChatWindow() {
           <ul className="space-y-2">
             {messages.map((message, index) => (
               <li key={index} className={`flex ${index % 2 === 0 ? 'justify-end' : 'justify-start'}`}>
-                <div className={`bg-gray-200 rounded-lg p-2 ${index % 2 === 0 ? 'ml-2' : 'mr-2'}`}>
+                <div className={`rounded-lg p-2 ${index % 2 === 0 ? 'bg-green-200 ml-2' : 'bg-blue-200 mr-2'}`}>
                   {message}
                 </div>
               </li>
@@ -37,7 +68,9 @@ export default function ChatWindow() {
           </ul>
           <form onSubmit={handleFormSubmit} className="mt-4 flex">
             <input type="text" value={inputValue} onChange={handleInputChange} className="border border-gray-400 p-2 rounded-lg w-full" />
-            <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-lg ml-2">Send</button>
+            <button type="submit" disabled={isSending} className="bg-blue-500 text-white px-4 py-2 rounded-lg ml-2">
+              {isSending ? '🧠 Thinking' : 'Send'}
+        </button>
           </form>
         </div>
       </div>
