13555d760da92570aa451ce210c449c09e248161
Achen2089 <anthonychen2089@gmail.com>
2023-12-04 10:14:13 -0800

    adjust componenets

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from "react";
 import CodeEditor from "./CodeEditor";
 import axios from "axios";
 
-import { classnames } from "../utils/general";
 import { languageOptions } from "../constants/languageOptions";
+import { classnames } from "../utils/general";
 
 import { ToastContainer, toast } from "react-toastify";
+import Split from "react-split";
 import "react-toastify/dist/ReactToastify.css";
 
 import ChatWindow from "./ChatWindow";
@@ -136,42 +137,41 @@ const MainPage: React.FC = () => {
         draggable
         pauseOnHover
       />
-      <div className="h-4 w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"></div>
-      <div className="flex flex-row">
-        <div className="px-4 py-2">
-          <LanguagesDropdown onSelectChange={onSelectChange} />
-        </div>
-      </div>
-      <div className="flex flex-row space-x-4 items-start px-4 py-4">
+      <Split className="flex flex-row" sizes={[33, 67]} minSize={100} gutterSize={10}>
         <div> 
           <ChatWindow />
         </div>
-        <div className="flex flex-col w-full h-full justify-start items-end">
-            <CodeEditor
-              code={code}
-              onChange={onChange}
-              language={language?.value}
-              theme={'vs-dark'}
-            />
-        </div>
-        <div className="right-container flex flex-shrink-0 w-[30%] flex-col">
-          <OutputWindow outputDetails={outputDetails} />
-          <div className="flex flex-col items-end">
+        <div className="w-full">
+          <div className="flex flex-row justify-between px-4 py-2">
+            <LanguagesDropdown onSelectChange={onSelectChange} />
             <button
               onClick={handleCompile}
               disabled={!code}
               className={classnames(
-                "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
+                "border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white",
                 !code ? "opacity-50" : ""
-              )}
-            >
+              )}>
               {processing ? "Processing..." : "Compile and Execute"}
             </button>
           </div>
-          {outputDetails && <OutputDetails outputDetails={outputDetails} />}
-        </div>  
-      </div>
+          <Split className="flex-grow" direction="vertical" sizes={[66, 34]} minSize={50} gutterSize={10}>
+            <div>
+              <CodeEditor
+                code={code}
+                onChange={onChange}
+                language={language?.value}
+                theme={'vs-dark'}
+              />
+            </div>
+            <div>
+              <OutputWindow outputDetails={outputDetails} />
+              {outputDetails && <OutputDetails outputDetails={outputDetails} />}
+            </div>
+          </Split>
+        </div>
+      </Split>
     </>
+
   );
 };
 
