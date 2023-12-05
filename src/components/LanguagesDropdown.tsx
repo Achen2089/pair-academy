33e5a2f354cc0851787dbc09c9a41e89f2fd7910
Achen2089 <anthonychen2089@gmail.com>
2023-12-05 11:07:03 -0800

    update dropdown

--- src/components/LanguagesDropdown.tsx
+++ src/components/LanguagesDropdown.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Select from "react-select";
-import { customStyles } from "../constants/customStyles";
 import { languageOptions } from "../constants/languageOptions";
 
 type LanguagesDropdownProps = {
@@ -10,9 +9,9 @@ type LanguagesDropdownProps = {
 const LanguagesDropdown = ({ onSelectChange }: LanguagesDropdownProps) => {
   return (
     <Select
+      className="bg-white border-gray-300 text-gray-700 py-2 px-4 rounded-lg shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500"
       placeholder={`Filter By Category`}
       options={languageOptions}
-      styles={customStyles}
       defaultValue={languageOptions[0]}
       onChange={(selectedOption) => onSelectChange(selectedOption)}
     />
