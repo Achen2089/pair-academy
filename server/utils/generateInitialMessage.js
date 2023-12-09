b47a9d2286acaa28f48ed1afaebbe5db17171d15
Achen2089 <anthonychen2089@gmail.com>
2023-12-08 16:44:08 -0800

    prompt engineering updates

--- server/utils/generateInitialMessage.js
+++ server/utils/generateInitialMessage.js
@@ -1,9 +1,20 @@
-function generateInitialMessage(lesson, programmingLanguage) {
+function generateInitialMessage(lesson, language) {
     return `
-    Welcome to our lesson on "${lesson.title}"! Today, we will delve into: ${lesson.details.description}
-    We have planned the following activities to enhance your learning: ${lesson.details.activities.join(', ')}. These activities are designed to give you practical experience and reinforce the concepts we'll cover.
-    Feel free to ask questions and interact as much as you need. Let's start exploring the fascinating world of ${programmingLanguage} programming with this topic. Are you ready to get started?
-    `;
+    ## Welcome to our lesson on "${lesson.title}"!
+
+    Today, we will delve into:
+    
+    ${lesson.details.description}
+    
+    We have planned the following activities to enhance your learning:
+    
+    ${lesson.details.activities.map((activity) => `* ${activity}`).join('\n')}
+    
+    These activities are designed to give you practical experience and reinforce the concepts we'll cover.
+    
+    Feel free to ask questions and interact as much as you need. Let's start exploring the fascinating world of ${language} programming with this topic.
+    
+    Are you ready to get started?`;
 }
 
 export default generateInitialMessage;
--- server/utils/generateTutorPrompt.js
+++ server/utils/generateTutorPrompt.js
@@ -1,17 +1,17 @@
-function generateTutorPromptForlesson(lesson, programmingLanguage) {
+function generateTutorPromptForlesson(lesson, language) {
     return `
     --- Tutor Mode: Programming for Beginners ---
 
     lesson: ${lesson.title}
     Description: ${lesson.details.description}
     Activities: ${lesson.details.activities.join(', ')}
-    Programming Language: ${programmingLanguage}
+    Programming Language: ${language}
 
     Welcome to our lesson on "${lesson.title}"! Today, we will delve into: ${lesson.details.description}
 
     We have planned the following activities to enhance your learning: ${lesson.details.activities.join(', ')}. These activities are designed to give you practical experience and reinforce the concepts we'll cover.
 
-    Feel free to ask questions and interact as much as you need. Let's start exploring the fascinating world of ${programmingLanguage} programming with this topic. Are you ready to get started?
+    Feel free to ask questions and interact as much as you need. Let's start exploring the fascinating world of ${language} programming with this topic. Are you ready to get started?
 
     ---
     `;
