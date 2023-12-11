fa8341405d4db54920c074bb2b48a0a177252a0e
Achen2089 <anthonychen2089@gmail.com>
2023-12-10 16:23:05 -0800

    homepage changes

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
         <div className="bg-blue-600 h-screen flex justify-center items-center">
             <div className="text-center">
                 <h1 className="text-6xl text-white font-bold mb-6">Welcome to Pair Academy</h1>
-                <p className="text-xl text-white mb-8">Your personal assistant for learning coding</p>
+                <p className="text-xl text-white mb-8">Your personal tutor for learning how to code</p>
                 <Link to="/code-editor">
                     <button className="bg-green-500 text-white font-bold py-4 px-8 rounded-full hover:bg-green-600 transition duration-300 ease-in-out text-2xl">
                         Get Started
